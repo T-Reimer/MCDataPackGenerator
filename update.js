21aea1500873cc48bda1346adb3b9196d5e32e11
T-Reimer <trevorreimer95@gmail.com>
2018-09-22 09:46:11 -0300

    added some comments and npm run dev

--- update.js
+++ update.js
@@ -1,8 +1,10 @@
 const nrc = require("node-run-cmd");
 
+// commands to run when installing or updating
 const commands = [
     "npm install",
     { command: 'git clone https://github.com/google/blockly.git', cwd: './repos' }
 ];
 
+// run the commands
 nrc.run(commands);
